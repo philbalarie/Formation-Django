94c1df58ff46b0831194125b2f4ca1ebe2c66784
philbalarie <philbalarie@gmail.com>
2020-04-15 16:10:48 -0400

    Intégration de base du thème et de la page d'accueil

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/home-page.html')
 
 def about(request):
     return render(request, 'pages/about.html')
