45dc81e42e8abf1767ace2535e9e485f65fcd591
philbalarie <philbalarie@gmail.com>
2020-04-18 16:33:57 -0400

    Ajout du template tags pour la nav

---
+++ pages/templatetags/cart_template_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from travels.models import Order
+
+register = template.Library()
+
+@register.filter
+def cart_travel_count(user):
+    if user.is_authenticated:
+        order_qs = Order.objects.filter(user=user, ordered=False)
+        if order_qs.exists():
+            return order_qs[0].travels.count()
--- travelagency/settings.py
+++ travelagency/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'pages',
     'accounts',
     'travels',
+    'shop',
 ]
 
 MIDDLEWARE = [
